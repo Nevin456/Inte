00df8490785c95f1af8c90cbd7ab0430ab46579e
nvins <nvwijetunga@students.nsbm.lk>
2021-03-03 03:00:54 +0530

    New

---
+++ app/src/main/java/com/example/smarttrashcanmap/Main.java
@@ -0,0 +1,30 @@
+package com.example.smarttrashcanmap;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Main extends AppCompatActivity {
+
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button=findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openMap();
+            }
+        });
+    }
+    public void openMap(){
+        Intent intent=new Intent(this,MapsActivity.class);
+        startActivity(intent);
+    }
+}
