a875acae1399ffbb7a555b933cd096329fe8e0ff
Mighara Cooray <bmacooray@students.nsbm.ac.lk>
2021-03-11 11:50:07 +0530

    Login finished

--- app/src/main/java/com/example/smarttrashcanmap/Main.java
+++ app/src/main/java/com/example/smarttrashcanmap/Main.java
@@ -7,9 +7,12 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.gms.common.SignInButton;
+
 public class Main extends AppCompatActivity {
 
     private Button button;
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,9 +25,26 @@ public class Main extends AppCompatActivity {
                 openMap();
             }
         });
+
+        /*button2=findViewById(R.id.SignIn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openLogin();
+            }
+        });
+*/
+
     }
+
     public void openMap(){
-        Intent intent=new Intent(this,MapsActivity.class);
-        startActivity(intent);
+        Intent intent1=new Intent(this,MapsActivity.class);
+        startActivity(intent1);
     }
+
+    public void openLogin()
+        {
+            Intent intent2 = new Intent(this, LoginActivity.class);
+            startActivity(intent2);
+        }
 }
