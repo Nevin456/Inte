51059f674b13992d1fe2e60665b52daa77131bd9
nvins <nvwijetunga@students.nsbm.lk>
2021-03-15 14:36:04 +0530

    Login database added

--- app/src/main/java/com/example/smarttrashcanmap/LoginActivity.java
+++ app/src/main/java/com/example/smarttrashcanmap/LoginActivity.java
@@ -1,14 +1,82 @@
 package com.example.smarttrashcanmap;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText mEmail, mPass;
+    private TextView mTextView;
+    private Button signInBtn;
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        mEmail = findViewById(R.id.inputusername);
+        mPass = findViewById(R.id.inputpass);
+        signInBtn = findViewById(R.id.buttonLogin);
+        mTextView = findViewById(R.id.textView2);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        signInBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUser();
+            }
+        });
+
     }
+    private void loginUser(){
+        String email = mEmail.getText().toString();
+        String pass = mPass.getText().toString();
+
+        if (!email.isEmpty() && Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            if (!pass.isEmpty()){
+                mAuth.signInWithEmailAndPassword(email , pass)
+                        .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                            @Override
+                            public void onSuccess(AuthResult authResult) {
+                                Toast.makeText(LoginActivity.this, "Login Successfully !!", Toast.LENGTH_SHORT).show();
+                                startActivity(new Intent(LoginActivity.this , MapsActivity.class));
+                                finish();
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(LoginActivity.this, "Login Failed !!", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }else{
+                mPass.setError("Empty Fields Are not Allowed");
+            }
+        }else if(email.isEmpty()){
+            mEmail.setError("Empty Fields Are not Allowed");
+        }else{
+            mEmail.setError("Pleas Enter Correct Email");
+        }
+    }
+
 }
