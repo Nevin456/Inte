dd32021d0c83e414246d8d8fc34f046a2fba4f99
nvins <nvwijetunga@students.nsbm.lk>
2021-03-15 22:13:06 +0530

    Interlinked2

--- app/src/main/java/com/example/smarttrashcanmap/splashscreenActivity.java
+++ app/src/main/java/com/example/smarttrashcanmap/splashscreenActivity.java
@@ -2,7 +2,9 @@ package com.example.smarttrashcanmap;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.ImageView;
 
 import com.airbnb.lottie.LottieAnimationView;
@@ -26,5 +28,16 @@ public class splashscreenActivity extends AppCompatActivity {
         logo.animate().translationY(2000).setDuration(3000).setStartDelay(4000);
         appName.animate().translationY(2000).setDuration(3000).setStartDelay(4000);
         lottieAnimationView.animate().translationY(2000).setDuration(3000).setStartDelay(4000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                final Intent mainIntent=new Intent(splashscreenActivity.this,Main.class);
+                splashscreenActivity.this.startActivity(mainIntent);
+                splashscreenActivity.this.finish();
+            }
+        },6000);
     }
+
+
 }
